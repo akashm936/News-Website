e9d2a527634b1097d75e0cf7b7a10866a5607046
Akash Morwal <114584845+akashm936@users.noreply.github.com>
2023-03-20 13:04:56 +0530

    Add files via upload

---
+++ News Website/script/index.js
@@ -0,0 +1,16 @@
+console.log("Script running...");
+document.querySelector('.cross').style.display = 'none'; 
+document.querySelector('.Hamburger').addEventListener("click", ()=>{
+   document.querySelector('.navbar').classList.toggle('navbarGo');
+   if(document.querySelector('.navbar').classList.contains('navbarGo')){
+      document.querySelector('.ham').style.display = 'inline';
+      document.querySelector('.cross').style.display = 'none';
+   }
+   else{
+      document.querySelector('.ham').style.display = 'none';
+      setTimeout(() =>{
+      document.querySelector('.cross').style.display = 'inline'; 
+      }, 300); 
+   }
+
+})
